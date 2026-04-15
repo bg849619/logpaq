cefbecb894e3a7e2799f8898d890e98bb012db68
Blake Gall <bg849619@ohio.edu>
2026-04-15 02:36:57 +0000

    Implemented test cases for vclock.

--- crdt/vclock_test.go
+++ crdt/vclock_test.go
@@ -23,6 +23,37 @@ func TestMerge(t *testing.T) {
 }
 
 // Test relationships between VClocks
-func TestRelation(t *testing.T) {
+func TestCompare(t *testing.T) {
+	// Base vClock
+	vc1 := VClock{"node-a": 1, "node-b": 2}
+	// vc2 should be after vc1. Therefore vc1 should be before vc2.
+	vc2 := VClock{"node-a": 2, "node-b": 3}
+	// vc3 should be concurrent with vc1
+	vc3 := VClock{"node-a": 2, "node-b": 1}
+	// vc4 should be equal to vc1
+	vc4 := VClock{"node-a": 1, "node-b": 2}
 
+	if vc1.Compare(vc2) != Before {
+		t.Errorf("Expected vc1 to be before vc2, got: %s", vc1.Compare(vc2))
+	}
+	if vc2.Compare(vc1) != After {
+		t.Errorf("Expected vc2 to be after vc1, got: %s", vc2.Compare(vc1))
+	}
+	if vc1.Compare(vc3) != Concurrent {
+		t.Errorf("Expected vc1 to be concurrent with vc3, got: %s", vc1.Compare(vc3))
+	}
+	if vc1.Compare(vc4) != Equal {
+		t.Errorf("Expected vc1 to be equal to vc4, got: %s", vc1.Compare(vc4))
+	}
+}
+
+func TestDelta(t *testing.T) {
+	vc1 := VClock{"node-a": 1, "node-b": 2}
+	vc2 := VClock{"node-a": 3, "node-b": 1}
+
+	delta := vc1.Delta(vc2)
+
+	if delta["node-a"] != 2 || delta["node-b"] != 0 {
+		t.Errorf("Delta calculation is incorrect, got: %v", delta)
+	}
 }
